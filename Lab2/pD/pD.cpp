42e930140cd6526168ae06609a8dd394f0594992
ModernHuman0531 <dunkun130897441@gmail.com>
2025-10-14 21:52:36 +0800

    Finish lab2 pD final version(segement tree)

--- Lab2/pD/pD.cpp
+++ Lab2/pD/pD.cpp
@@ -1,35 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define lc 2*id
+#define rc 2*id+1
 int n;
 vector<long long> vec;
-vector<long long> prefix_sum;
-void prefix_cal(int n);
-void Update(int p,int x);
-void Query(int l,int r);
+vector<long long> ex;
+void build(int L,int R,int id);
+long long query(int l,int r,int L,int R,int id);
+void modify(int p,int x,int L,int R,int id);
 void operation(int i,int j,int k){
     // i is operation,j and k are the input of Update/Query
-    if(i==1) return Update(j,k);
-    if(i==2) return Query(j,k);
+    if(i==1) return modify(j,k,1,n,1);
+    if(i==2){
+        long long ans=query(j,k,1,n,1);
+        cout<<ans<<"\n";
+        return;
+    } 
 
 }
-void Update(int p,int x){
-    vec[p]=x;
-    prefix_cal(n);
-    return;
-}
-void Query(int l,int r){
-    long long ans=prefix_sum[r]^prefix_sum[l-1];
-    cout<<ans<<"\n";
-}
-void prefix_cal(int n){
-    prefix_sum.clear();
-    prefix_sum.push_back(0); // prefix_sum[0] = 0
-    long long sum=0;
-    for(int i=1;i<=n;++i){
-        sum=sum^vec[i];
-        prefix_sum.push_back(sum);
+void build(int L,int R,int id){
+    if(L==R){// Base case
+        ex[id]=vec[L];
+        return;
     }
+    int M=(L+R)/2;
+    build(L,M,lc); // Build left subtree
+    build(M+1,R,rc); //Build right subtree
+    ex[id]=ex[lc]^ex[rc];
+}
+
+long long query(int l,int r,int L,int R,int id){
+    int M=(L+R)/2;
+    if(l<=L&&R<=r){ // binary tree node are in the target range, just take that node's info
+         return ex[id];
+    }
+    if(r<=M){
+        return query(l,r,L,M,lc);   
+    }
+    if(M<l){
+        return query(l,r,M+1,R,rc);
+    }
+    else{
+        return (query(l,r,L,M,lc)^query(l,r,M+1,R,rc));
+    }
+}
+void modify(int p,int x,int L,int R,int id){
+    if(L==R){
+        ex[id]=x;
+        return;
+    }
+    int M=(L+R)/2;
+    if(p<=M){
+        modify(p,x,L,M,lc);
+    }
+    if(M<p){
+        modify(p,x,M+1,R,rc);
+    }
+    ex[id]=ex[lc]^ex[rc];
 }
 int main(){
     int q;
@@ -41,8 +68,8 @@ int main(){
         cin>>input;
         vec.push_back(input);
     }
-    // 初始計算 prefix sum
-    prefix_cal(n);
+    ex.resize(4*n+1); //Segment tree size normally need 4*n space to store data
+    build(1,n,1);
     
     cin>>q;
     for(int i=0;i<q;++i){
