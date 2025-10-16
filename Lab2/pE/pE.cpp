3b25fa573dde6d4b892c88806d992610e22d11b3
ModernHuman0531 <dunkun130897441@gmail.com>
2025-10-16 18:44:52 +0800

    Lab2 pE subtask1 pass

--- Lab2/pE/pE.cpp
+++ Lab2/pE/pE.cpp
@@ -3,28 +3,43 @@ using namespace std;
 class spaceNode{
     public:
         int x,y,z;
-        spaceNode(int x_,int y_,int z_):x{x_},y{y_},z{z_}{}
+        int idx;
+        spaceNode(int x_,int y_,int z_,int idx_):x{x_},y{y_},z{z_},idx(idx_){}
 };
+bool cmp(spaceNode x1,spaceNode x2){
+    return x1.x>x2.x;
+}
 vector<spaceNode> vec;
+vector<int> ans;
 int main(){
     ios::sync_with_stdio(false),cin.tie(nullptr);
     // Input
     int N;
     cin>>N;
-    for(int i=0;i<N;++i){
+    for(int i=1;i<=N;++i){
         int x,y,z;
         cin>>x>>y>>z;
-        vec.push_back(spaceNode(x,y,z));
+        vec.push_back(spaceNode(x,y,z,i));
     }
-    for(int i=0;i<N;++i){
-        int count=0;
-        for(int j=0;j<N;++j){
-            // Can write operator overloading
-            if((vec[i].x<vec[j].x)&&(vec[i].y<vec[j].y)&&(vec[i].z<vec[j].z))
-                count++;
+    sort(vec.begin(),vec.end(),cmp);
+    ans.resize(N+1);
+    ans.push_back(0);
+    // Use double pointer to calculate the repeat number
+    int i=0;
+    while(i<N){
+        int j=i;
+        while(j<N&&vec[i].x==vec[j].x){
+            ++j;
         }
-        cout<<count<<"\n";
+        // The interval of same x value is [i,j)
+        for(int k=i;k<j;++k){
+            ans[vec[k].idx]=i;
+        }
+        i=j;
+    }
+    // Print out the ans vector
+    for(int i=1;i<=N;++i){
+       cout<<ans[i]<<"\n";
     }
     return 0;
-
 }
