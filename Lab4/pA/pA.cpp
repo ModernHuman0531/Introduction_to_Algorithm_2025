03b6f456e1c6b389ef479e399c019c06176f9afa
ModernHuman0531 <dunkun130897441@gmail.com>
2025-11-25 17:39:41 +0800

    Lab4 pA subtask1,2 rolling dp

--- Lab4/pA/pA.cpp
+++ Lab4/pA/pA.cpp
@@ -4,7 +4,7 @@ const int MAXN=1e4;
 const int MAXW=100;
 const long long INF=MAXN*1000000000ll;
 
-long long dp[MAXN][MAXW];
+long long dp[MAXW];
 int ai[MAXN],bi[MAXN],ci[MAXN];
 int main(){
     ios::sync_with_stdio(false),cin.tie(nullptr);
@@ -13,26 +13,19 @@ int main(){
     for(int i=1;i<=n;++i){
         cin>>ai[i]>>bi[i]>>ci[i];
     }
-
     // Initialization, dp[0][0] means put nothing in it, the current used space is 0
-    dp[0][0]=0;
+    dp[0]=0;
     // 0 item can't have i size
     for(int i=1;i<=w;++i){
-        dp[0][i]=-INF;
+        dp[i]=-INF;
     }
-
+    int now=1;
     for(int i=1;i<=n;++i){
-        // If current space can't put item i, then don't put it, the state stay the same
-        for(int j=0;j<ai[i];++j){
-            dp[i][j]=dp[i-1][j];
-        }
-        // If current space can put item i, we choose the maximum value of put or not put
-        for(int j=ai[i];j<=w;++j){
-            dp[i][j]=max(dp[i-1][j],dp[i-1][j-ai[i]]+bi[i]);
+        for(int j=w;j>=ai[i];--j){
+            dp[j]=max(dp[j],dp[j-ai[i]]+bi[i]);
         }
     }
-
     // Choose the maximum value consider the first n element
-    cout<<*max_element(dp[n],dp[n]+w+1)<<"\n";
+    cout<<*max_element(dp,dp+w+1)<<"\n";
     return 0;
 }
